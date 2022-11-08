2aeca0b87834363a432ac8c870c68cb190358cc3
Richd0tcom <tuberich0@gmail.com>
2022-11-08 16:53:07 +0100

    config: set mysql vars

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -8,8 +8,9 @@ const db = new Sequelize(
   process.env.DB_USERNAME as string,
   process.env.DB_PASSWORD,
   {
-    host: "localhost",
+    host: process.env.DB_HOST,
     dialect: "mysql",
+    port: 3306,
   }
 );
 
